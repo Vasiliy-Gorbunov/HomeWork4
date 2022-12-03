f4aa25bc7a1dab21635f092b6f69d2f7335c3575
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-03 13:05:29 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Поздравляем, вы совершеннолетний!");
+        }
+        if (age < 18)  {
+            System.out.println("Вы пока не достигли совершеннолетия, нужно немного подождать.");
+        }
     }
 }
