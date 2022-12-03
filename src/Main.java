60c9d12831ae61fff59d4da385981eaa5591380e
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-03 13:16:11 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
         int age = 25;
         if (age >= 18) {
             System.out.println("Поздравляем, вы совершеннолетний!");
         }
         if (age < 18)  {
             System.out.println("Вы пока не достигли совершеннолетия, нужно немного подождать.");
+        }
+        //Задание 2
+        int yearsOld = 25;
+        if (yearsOld < 7) {
+            System.out.println("Ребёнку пока рано в школу");
+        }
+        if (yearsOld >=7 && yearsOld < 18) {
+            System.out.println("Ребёнок ходит в школу");
+        }
+        if (yearsOld >=18 && yearsOld < 24) {
+            System.out.println("Ученик закончил школу и может поступить в университет");
+        }
+        if (yearsOld > 24) {
+            System.out.println("Человеку пора искать первую работу");
         }
     }
 }
