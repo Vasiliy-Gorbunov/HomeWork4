555afdb6d52992c9b8a42a43d67a69ddc61c872e
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-03 13:42:11 +0300

    task 1-3 with "else"

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,31 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         System.out.println("\nЗадание 1:");
-        int age = 25;
+        int age = 15;
         if (age >= 18) {
             System.out.println("Поздравляем, вы совершеннолетний!");
         }
-        if (age < 18)  {
+            else {
             System.out.println("Вы пока не достигли совершеннолетия, нужно немного подождать.");
-        }
+            }
+
         //Задание 2
         System.out.println("\nЗадание 2:");
         int yearsOld = 25;
         if (yearsOld < 7) {
             System.out.println("Ребёнку пока рано в школу");
+        }else {
+            if (yearsOld >= 7 && yearsOld < 18) {
+                System.out.println("Ребёнок ходит в школу");
+            } else {
+                if (yearsOld >= 18 && yearsOld < 24) {
+                    System.out.println("Ученик закончил школу и может поступить в университет");
+                } else {
+                    System.out.println("Человеку пора искать первую работу");
+                }
+            }
         }
-        if (yearsOld >=7 && yearsOld < 18) {
-            System.out.println("Ребёнок ходит в школу");
-        }
-        if (yearsOld >=18 && yearsOld < 24) {
-            System.out.println("Ученик закончил школу и может поступить в университет");
-        }
-        if (yearsOld > 24) {
-            System.out.println("Человеку пора искать первую работу");
-        }
+
         //Задание 3
         System.out.println("\nЗадание 3:");
         int summPlaces = 102;
@@ -35,9 +38,10 @@ public class Main {
         int actualStandPlaces = standPlaces-actualStandPeoples;
         if (actualSitPlaces > 0 || actualStandPlaces > 0) {
             System.out.println("В вагоне осталось "+actualSitPlaces+" сидячих мест и "+actualStandPlaces+" стоячих мест.");
-        }
-        if (actualSitPlaces == 0 && actualStandPlaces == 0) {
+        }else {
             System.out.println("Вагон полностью заполнен!");
         }
+
+
     }
 }
